323fd9eac61ecb46ac5d5be7ede3845779339c7e
937miaow <88704063+937miaow@users.noreply.github.com>
2025-09-12 19:48:09 +0800

    feat(user/sleep.c): add error check and --help support

--- user/sleep.c
+++ user/sleep.c
@@ -4,12 +4,38 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc < 2) // lack of ticks
     {
-        fprintf(2, "Usage: sleep ticks\n");
+        fprintf(2, "sleep: lack of ticks\n"
+                   "please use 'sleep --help' for more information\n");
         exit(1);
     }
+
+    if (strcmp(argv[1], "--help") == 0) // help message
+    {
+        fprintf(2, "Usage: sleep ticks\n"
+                   "Pause execution for a specified number of clock ticks.\n"
+                   "Example: sleep 100 (sleeps for 100 clock ticks)\n");
+        exit(1);
+    }
+
+    for (char *p = argv[1]; *p; p++) // check if argv[1] is a valid number
+    {
+        if (*p < '0' || *p > '9')
+        {
+            fprintf(2, "sleep: invalid number of ticks\n");
+            exit(1);
+        }
+    }
+
     int ticks = atoi(argv[1]);
+
+    if (ticks < 0) // negative ticks
+    {
+        fprintf(2, "sleep: invalid number of ticks\n");
+        exit(1);
+    }
+
     sleep(ticks);
     exit(0);
 }
